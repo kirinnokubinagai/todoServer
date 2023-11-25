2e7be367c06edc02cdb7c9f093371b14386ab0fd
柴田大介 <kirin@shibatadaisukenoMacBook-Pro.local>
2023-11-25 21:21:42 +0900

    .corsのドメイン追加

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const PORT = 8000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN_DOMAIN,
   optionsSuccessStatus: 200,
 };
 
